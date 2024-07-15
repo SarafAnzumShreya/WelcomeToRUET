c9543f207e41c862dbe6365da234770ebb130868
Saraf Anzum Shreya <96836008+SarafAnzumShreya@users.noreply.github.com>
2024-07-15 16:04:14 +0600

    Update front_page.js

--- front_page.js
+++ front_page.js
@@ -1,19 +1,21 @@
-document.getElementById('form').addEventListener('submit', function(event) {
-    event.preventDefault();
-
-    document.getElementById('popUp').style.display="block";
-    document.getElementById('continue').style.display="block";
-    document.getElementById('board').style.display="none";
-    document.getElementById('text').style.display="none";
-    document.getElementById('wall').style.display="none";
-    document.getElementById('shadow').style.display="none";
-});
-
-document.getElementById('continue').addEventListener('click', function(event) {
-    event.preventDefault();
-
-    const name = document.getElementById('name').value;
-    const dept = document.getElementById('department').value;
-
-    window.location.href = `main_game.html?name=${encodeURIComponent(name)}&dept=${encodeURIComponent(dept)}`;
+    document.addEventListener("DOMContentLoaded", () => {
+        document.getElementById('form').addEventListener('submit', function(event) {
+        event.preventDefault();
+    
+        document.getElementById('popUp').style.display="block";
+        document.getElementById('continue').style.display="block";
+        document.getElementById('board').style.display="none";
+        document.getElementById('text').style.display="none";
+        document.getElementById('wall').style.display="none";
+        document.getElementById('shadow').style.display="none";
+    });
+    
+    document.getElementById('continue').addEventListener('click', function(event) {
+        event.preventDefault();
+    
+        const name = document.getElementById('name').value;
+        const dept = document.getElementById('department').value;
+    
+        window.location.href = `main_game.html?name=${encodeURIComponent(name)}&dept=${encodeURIComponent(dept)}`;
+    });
 });
